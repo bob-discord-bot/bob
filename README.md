4cee40cfe2ae592f471bea9083a98eaa2f8c703f
omame <me@omame.tech>
2021-06-05 01:36:09 +0200

    v2.2.0 alpha woozy

--- README.md
+++ README.md
@@ -1,22 +1,9 @@
 # bob v2
-bob is a chatbot designed for use in TFT
+bob is a chat-bot 
 
 ## how to use
 1. install the requirements
-2. copy `config.example.json` to `config.json` and edit it
-2. run main.py
+2. put token in `token.txt`
+3. run main.py
 
 is it hard/??????
-
-## config options
-### `scrape_channel`
-channel which bob will scrape for data
-
-### `target_channel`
-channel in which bob will be talking in
-
-### `debug_channel`
-channel in which bob will be talking in when in debug mode
-
-### `scrape_amount`
-amount of messages bob will go through for its dataset
--- main.py
+++ main.py
@@ -1,65 +1,124 @@
-from qna import helpers, json, classes
+import os
+import qna
 import argparse
 import discord
-from configreader import config
+from discord.ext import commands, tasks
+import json
 
-version = 'v2.2.0 beta'
+version = "v2.2.0 alpha"
 
-parser = argparse.ArgumentParser(description=f'bob {version}')
-parser.add_argument('--debug', '-d', action='store_true', help='enable debug mode')
-parser.add_argument('--version', '-v', action='version', version=version)
+parser = argparse.ArgumentParser(description=f"bob {version}")
+parser.add_argument("--debug", "-d", action="store_true", help="enable debug mode (currently unused)")
 args = parser.parse_args()
 
-channel_id = config["debug_channel"] if args.debug else config["target_channel"]
-client = discord.Client()
-questions = []
-
-
-def debug(*arg, **kwargs):
-    if args.debug:
-        print(*arg, **kwargs)
-
-
-async def debug_send(message: discord.Message, *arg, **kwargs):
-    if args.debug:
-        await message.channel.send(*arg, **kwargs)
+client = commands.Bot(command_prefix="bob.")
+question_map = {}
+config = {"guilds": {}}
 
 
 @client.event
 async def on_ready():
-    print(f'bob {version} is ready!')
-    channel = client.get_channel(channel_id)
-    await channel.send(f'ℹ️ **bob {version} is ready** with {len(questions)} questions in the dataset')
-    await channel.edit(topic=f'**bob {version}** | trained on {len(questions)} questions')
+    print(f"bob {version} is ready!")
+    periodic_data_save.start()
+    update_status.start()
+
+
+@client.event
+async def on_disconnect():
+    print(f"client disconnected, saving data...", end=" ")
+    with open("config.json", "w+") as file:
+        json.dump(config, file)
+
+    with open("data.json", "w+") as file:
+        file.write(qna.json.questions_to_json(list(question_map.values())))
+
+    print("done.")
+
+
+@tasks.loop(minutes=5.0)
+async def periodic_data_save():
+    print(f"saving data...", end=" ")
+    with open("config.json", "w+") as file:
+        json.dump(config, file)
+
+    with open("data.json", "w+") as file:
+        file.write(qna.json.questions_to_json(list(question_map.values())))
+
+    print("done.")
+
+
+@tasks.loop(seconds=15.0)
+async def update_status():
+    game = discord.Activity(
+        type=discord.ActivityType.listening,
+        name=f"{len(question_map.keys())} questions // bob.help"
+    )
+    await client.change_presence(activity=game)
 
 
 @client.event
 async def on_message(message: discord.Message):
-    if message.channel.id != channel_id:
+    if message.author.bot or message.is_system() or message.guild is None:
         return
-    if message.author.bot:
-        return
-    content = classes.sanitize_question(message.clean_content)
-    if message.is_system():
-        content = classes.sanitize_question(message.system_content)
-    question = helpers.get_closest_question(questions, content)
-    response = helpers.pick_response(question)
-    debug(question)
-    debug(content, '->', response.text)
-    embed = discord.Embed(title="Details", color=0x2273e6)
-    embed.add_field(name="Matched Question", value=question.text)
-    embed.add_field(name="# of Responses", value=len(question.responses))
-    try:
-        await message.reply(response.text, embed=embed)
-    except discord.errors.HTTPException:
-        await message.channel.send(response.text, embed=embed)
+
+    await client.process_commands(message)
+
+    if str(message.guild.id) in config["guilds"].keys():
+        if message.channel.id == config["guilds"][str(message.guild.id)]["channel"]:
+            content = qna.classes.sanitize_question(message.clean_content)
+            question = qna.helpers.get_closest_question(list(question_map.values()), content)
+            response = qna.helpers.pick_response(question)
+            try:
+                await message.reply(response.text)
+            except discord.errors.HTTPException:
+                return
+
+    if message.reference:
+        try:
+            reply = await message.channel.fetch_message(message.reference.message_id)
+        except discord.NotFound:
+            return
+
+        content = qna.classes.sanitize_question(reply.clean_content)
+        if content not in question_map.keys():
+            question_map.update({content: qna.classes.Question(content)})
+        question_map[content].add_response(qna.classes.Response(message.clean_content))
+        print(content, '->', message.clean_content)
 
 
-if __name__ == '__main__':
-    debug('initializing...')
-    with open('data.json') as file:
-        questions = json.json_to_questions(file.read())
-    debug('loaded', len(questions), 'questions')
+@commands.has_permissions(manage_channels=True)
+@client.command()
+async def channel(ctx: commands.Context, target_channel: discord.TextChannel):
+    if str(ctx.guild.id) not in config["guilds"].keys():
+        config["guilds"].update({str(ctx.guild.id): {"channel": target_channel.id}})
+    else:
+        config["guilds"][str(ctx.guild.id)]["channel"] = target_channel.id
 
-    with open('token.txt') as file:
+    await ctx.reply(f"done! bob will now talk in {target_channel.mention}.")
+
+
+@commands.is_owner()
+@client.command()
+async def stop(ctx: commands.Context):
+    await client.close()
+
+
+if __name__ == "__main__":
+    print("loading questions...")
+    with open("data.json") as file:
+        questions = qna.json.json_to_questions(file.read())
+    for question in questions:
+        question_map.update({question.text: question})
+    del questions
+    print("loaded", len(question_map.keys()), "questions")
+
+    print("loading config...")
+    if os.path.exists("config.json"):
+        with open("config.json") as file:
+            config = json.load(file)
+            if "guilds" not in config.keys():
+                config.update({"guilds": {}})
+
+    print("connecting to discord...")
+    with open("token.txt") as file:
         client.run(file.readline())
