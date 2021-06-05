57e40ae6865c0740a6084a1b6b294101c3521fed
omame <me@omame.tech>
2021-06-05 02:23:24 +0200

    add server count to status

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ async def periodic_data_save():
 async def update_status():
     game = discord.Activity(
         type=discord.ActivityType.listening,
-        name=f"{len(question_map.keys())} questions // bob {version} // bob.help"
+        name=f"{len(question_map.keys())} questions in {len(client.guilds)} servers // bob {version} // bob.help"
     )
     await client.change_presence(activity=game)
 
