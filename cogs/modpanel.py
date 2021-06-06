dee332ba4f70c21c21a9edb2dc3e9207e7677b59
omame <me@omame.tech>
2021-06-06 17:26:24 +0200

    more pain

--- cogs/modpanel.py
+++ cogs/modpanel.py
@@ -11,24 +11,43 @@ class ModPanel(commands.Cog):
         self.client = client
         self.logger = logging.getLogger("cogs.ModPanel")
         self.logger.debug("registered.")
-
+        self.config: Config = client.get_cog("Config")
         self.app = Flask("ModPanel")
 
         @self.app.route("/")
         def index():
             self.logger.debug("calculating responses...")
-            config: Config = client.get_cog("Config")
-            responses = len([response for question in config.question_map.values()
+            responses = len([response for question in self.config.question_map.values()
                              for response in question.responses])
             self.logger.debug("%d responses", responses)
             return render_template(
                 'index.html',
                 bob_version=bob.__version__,
-                questions=len(config.question_map.keys()),
+                questions=len(self.config.question_map.keys()),
                 responses=responses,
                 guilds=len(self.client.guilds),
                 users=len(self.client.users),
                 shards=self.client.shard_count
+            )
+
+        @self.app.route("/questions")
+        def question_list():
+            return render_template(
+                'question_list.html',
+                bob_version=bob.__version__,
+                questions=list(self.config.question_map.values())
+            )
+
+        @self.app.route("/question/<question_id>")
+        def question_manage(question_id):
+            question_id = int(question_id)
+            question_key = list(self.config.question_map.keys())[question_id]
+            question = self.config.question_map[question_key]
+            return render_template(
+                'question_manage.html',
+                bob_version=bob.__version__,
+                question=question,
+                id=question_id
             )
 
         self.process = threading.Thread(
