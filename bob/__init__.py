48ec61840fb649b2adfd190b588c03271b13a5f6
omame <me@omame.xyz>
2022-09-06 23:50:47 +0200

    chore: bump version, add some constants

--- bob/__init__.py
+++ bob/__init__.py
@@ -1,4 +1,12 @@
 """
 Constants used by bob.
 """
-__version__ = "2.3.0"
+__version__ = "2.3.1 alpha.1"
+
+CATCHPHRASES = [
+    "Your conversation partner.",
+    "Check us out on Top.gg!",
+    "A bit dumb and a bit smart."
+]
+
+SEPARATOR = '•'
