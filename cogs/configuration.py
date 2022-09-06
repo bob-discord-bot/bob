e75af84d3863ad79c216eb7121879021dd110698
omame <me@omame.xyz>
2022-09-06 23:45:37 +0200

    chore: remove channel topic updates
    closes #2

--- cogs/configuration.py
+++ cogs/configuration.py
@@ -13,7 +13,6 @@ class Configuration(commands.Cog):
         self.logger = logging.getLogger("cogs.Configuration")
         self.config: typing.Union[Config, None] = client.get_cog("Config")
         self.logger.debug("registered.")
-        self.reset_channel_topics.start()
 
     @commands.has_permissions(manage_channels=True)
     @commands.command(brief="Sets the channel that bob will talk in.")
@@ -23,24 +22,7 @@ class Configuration(commands.Cog):
             self.config.config["guilds"].update({str(ctx.guild.id): {"channel": target_channel.id}})
         else:
             self.config.config["guilds"][str(ctx.guild.id)]["channel"] = target_channel.id
-
-        await self.update_channel_topic(self.client.get_channel(target_channel.id))
         await ctx.reply(f"Bob will now talk in {target_channel.mention}.")
-
-    async def update_channel_topic(self, channel: discord.TextChannel):
-        try:
-            await channel.edit(topic=f"talk to bob! | "
-                                     f"run **{self.client.command_prefix}help** in an another channel!")
-        except discord.Forbidden:
-            self.logger.debug(f"I don't have permissions to update {channel}.")
-
-    @tasks.loop(hours=1)
-    async def reset_channel_topics(self):
-        for guild_id, data in self.config.config["guilds"].items():
-            if "channel" in data.keys():
-                channel = self.client.get_channel(int(data["channel"]))
-                if channel is not None:
-                    await self.update_channel_topic(channel)
 
 
 async def setup(client: commands.Bot):
