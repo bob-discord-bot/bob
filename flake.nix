0a6efe8b8e932f939721f5a5b6308fe0adbb8c2b
Victor B. <contact@itsvic.dev>
2025-10-20 19:33:49 +0200

    remove jishaku, bump nixpkgs, move to latest py3 and pray

--- flake.nix
+++ flake.nix
@@ -2,27 +2,46 @@
   description = "bob - a plug-n-play Discord chat bot";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: 
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      deps = pkgs.callPackage ./nix/dependencies.nix {};
-    in {
-      packages = rec {
-        bob = with pkgs.python3Packages; buildPythonApplication {
-          pname = "bob";
-          version = "3.0.1";
-          pyproject = true;
-          nativeBuildInputs = [ hatchling ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        deps = pkgs.callPackage ./nix/dependencies.nix { };
+      in {
+        packages = rec {
+          bob = with pkgs.python3Packages;
+            buildPythonApplication {
+              pname = "bob";
+              version = "3.0.1";
+              pyproject = true;
+              nativeBuildInputs = [ hatchling ];
 
-          propagatedBuildInputs = [
+              propagatedBuildInputs = [
+                discordpy
+                levenshtein
+                nest-asyncio
+                pynacl
+                deps.topggpy
+                deps.tortoiseORM
+                deps.aerich
+                pyyaml
+                tqdm
+              ];
+
+              src = ./.;
+            };
+          default = bob;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs.python3Packages; [
             discordpy
             levenshtein
             nest-asyncio
-            deps.jishaku
             pynacl
             deps.topggpy
             deps.tortoiseORM
@@ -30,26 +49,8 @@
             pyyaml
             tqdm
           ];
-
-          src = ./.;
         };
-        default = bob;
+      }) // {
+        hydraJobs = { inherit (self.packages.x86_64-linux) bob; };
       };
-
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs.python311Packages; [
-          discordpy
-          levenshtein
-          nest-asyncio
-          deps.jishaku
-          pynacl
-          deps.topggpy
-          deps.tortoiseORM
-          deps.aerich
-          pyyaml
-          tqdm
-        ];
-      };
-    }
-  );
 }
--- nix/dependencies.nix
+++ nix/dependencies.nix
@@ -1,126 +1,92 @@
 { pkgs, lib, fetchFromGitHub }:
 with pkgs.python3Packages;
 let
-    import_expression = buildPythonPackage rec {
-        pname = "import_expression";
-        version = "1.1.4";
+  import_expression = buildPythonPackage rec {
+    pname = "import_expression";
+    version = "1.1.4";
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-BghqarO/pSixxHjmM9at8rOpkOMUQPZAGw8+oSsGWak=";
+    };
+    doCheck = false;
+    propagatedBuildInputs = [ astunparse ];
+  };
+
+  pypikaTortoise = buildPythonPackage rec {
+    pname = "pypika-tortoise";
+    version = "0.1.6";
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-2AKGj0eacI4yY3JMe1cZomrXk5mypwzqBl9KTK2+vzY=";
+    };
+    pyproject = true;
+
+    nativeBuildInputs = [ poetry-core ];
+  };
+in rec {
+  topggpy = buildPythonPackage rec {
+    pname = "topggpy";
+    version = "1.4.0";
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-daLzARoHPAWCOLdAmJFVzIVZglDtYFt4i13ijEMU7AQ=";
+    };
+    doCheck = false;
+    pyproject = true;
+    build-system = [ setuptools ];
+    propagatedBuildInputs = [ aiohttp ];
+  };
+
+  tortoiseORM = buildPythonPackage rec {
+    pname = "tortoise-orm";
+    version = "0.20.0";
+    src = fetchPypi {
+      pname = "tortoise_orm";
+      inherit version;
+      sha256 = "sha256-KDr1hNaF3MWNbMHaNbkRW7HkHIkHXq4qGcSTs5ubQfc=";
+    };
+    doCheck = false;
+    pyproject = true;
+
+    nativeBuildInputs = [ poetry-core ];
+
+    propagatedBuildInputs = [
+      pypikaTortoise
+      (iso8601.overrideAttrs (old: rec {
+        version = "1.1.0";
         src = fetchPypi {
-            inherit pname version;
-            sha256 = "sha256-BghqarO/pSixxHjmM9at8rOpkOMUQPZAGw8+oSsGWak=";
+          pname = "iso8601";
+          inherit version;
+          hash = "sha256-MoEee4He7iBj6m0ulPiBmobR84EeSdI2I6QfqDK+8D8=";
         };
-        doCheck = false;
-        propagatedBuildInputs = [
-            astunparse
-        ];
-    };
-
-    pypikaTortoise = buildPythonPackage rec {
-        pname = "pypika-tortoise";
-        version = "0.1.6";
+      }))
+      (aiosqlite.overrideAttrs (old: rec {
+        version = "0.16.0";
         src = fetchPypi {
-            inherit pname version;
-            sha256 = "sha256-2AKGj0eacI4yY3JMe1cZomrXk5mypwzqBl9KTK2+vzY=";
+          pname = "aiosqlite";
+          inherit version;
+          sha256 = "1a0fjmlvadyzsml10g5p1qif7192k0swy5zwjp8v48y5zc3yy56h";
         };
-        pyproject = true;
+        propagatedBuildInputs = [ typing-extensions aiounittest ];
+      }))
+      pytz
+    ];
+  };
 
-        nativeBuildInputs = [
-            poetry-core
-        ];
+  aerich = buildPythonPackage rec {
+    pname = "aerich";
+    version = "2023-12-26";
+    src = fetchFromGitHub {
+      owner = "tortoise";
+      repo = "aerich";
+      rev = "ede53ade867f5e0a0a3f64c7363ab6decf5bd3c5";
+      hash = "sha256-8zW9NH4OI7N9FnhfqNkEgEvzhFTnwxDHDMu1K26pGZE=";
     };
-in
-rec {
-    jishaku = buildPythonPackage rec {
-        pname = "jishaku";
-        version = "2.5.2";
-        src = fetchPypi {
-            inherit pname version;
-            sha256 = "sha256-VtOMMzA243SB3148noHWAztQl3OPDRcageJ1ISTw31w=";
-        };
-        doCheck = false;
-        propagatedBuildInputs = [
-            braceexpand
-            click
-            import_expression
-            tabulate
-        ];
-    };
+    doCheck = false;
+    pyproject = true;
 
-    topggpy = buildPythonPackage rec {
-        pname = "topggpy";
-        version = "1.4.0";
-        src = fetchPypi {
-            inherit pname version;
-            sha256 = "sha256-daLzARoHPAWCOLdAmJFVzIVZglDtYFt4i13ijEMU7AQ=";
-        };
-        doCheck = false;
-        propagatedBuildInputs = [
-            aiohttp
-        ];
-    };
+    nativeBuildInputs = [ poetry-core ];
 
-    tortoiseORM = buildPythonPackage rec {
-        pname = "tortoise-orm";
-        version = "0.20.0";
-        src = fetchPypi {
-            pname = "tortoise_orm";
-            inherit version;
-            sha256 = "sha256-KDr1hNaF3MWNbMHaNbkRW7HkHIkHXq4qGcSTs5ubQfc=";
-        };
-        doCheck = false;
-        pyproject = true;
-        
-        nativeBuildInputs = [
-            poetry-core
-        ];
-
-        propagatedBuildInputs = [
-            pypikaTortoise
-            (iso8601.overrideAttrs(old: rec {
-                version = "1.1.0";
-                src = fetchPypi {
-                    pname = "iso8601";
-                    inherit version;
-                    hash = "sha256-MoEee4He7iBj6m0ulPiBmobR84EeSdI2I6QfqDK+8D8=";
-                };
-            }))
-            (aiosqlite.overrideAttrs(old: rec {
-                version = "0.16.0";
-                src = fetchPypi {
-                    pname = "aiosqlite";
-                    inherit version;
-                    sha256 = "1a0fjmlvadyzsml10g5p1qif7192k0swy5zwjp8v48y5zc3yy56h";
-                };
-                propagatedBuildInputs = [
-                    typing-extensions
-                    aiounittest
-                ];
-            }))
-            pytz
-        ];
-    };
-
-    aerich = buildPythonPackage rec {
-        pname = "aerich";
-        version = "2023-12-26";
-        src = fetchFromGitHub {
-            owner = "tortoise";
-            repo = "aerich";
-            rev = "ede53ade867f5e0a0a3f64c7363ab6decf5bd3c5";
-            hash = "sha256-8zW9NH4OI7N9FnhfqNkEgEvzhFTnwxDHDMu1K26pGZE=";
-        };
-        doCheck = false;
-        pyproject = true;
-        
-        nativeBuildInputs = [
-            poetry-core
-        ];
-
-        propagatedBuildInputs = [
-            tortoiseORM
-            click
-            pydantic
-            dictdiffer
-            tomlkit
-        ];
-    };
+    propagatedBuildInputs = [ tortoiseORM click pydantic dictdiffer tomlkit ];
+  };
 }
